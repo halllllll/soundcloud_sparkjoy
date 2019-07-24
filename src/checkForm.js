b5503dd21e28aac21687d77057f34ee2a5b878dd
halllllll <h4l@yahoo.ne.jp>
2019-07-24 19:00:52 +0900

    validation

--- src/checkForm.js
+++ src/checkForm.js
@@ -1,28 +1,43 @@
 export default class checkForm{
-  constructor(account_form, form_btn, label_alt) {
+  constructor(account_form, form_btn, label_alt, myform) {
     this.account_form = account_form;
     this.form_btn = form_btn;
     this.label_alt = label_alt;
-
+    this.myform = myform;
 
     this.account_form.addEventListener("input", (e) => {
-      // アンチパターン踏み抜き
+      // アンチパターン踏み抜きですかね
       const self = this;
       this.listenOn(self, e);
     });
 
+    this.myform.addEventListener("keypress", (e) => {
+      const self = this;
+      this.preventSubmit(self, e);
+    });
   }
+
+  preventSubmit(self, e) {
+    if (e.which == 13) {
+      e.preventDefault();
+      if (self.form_btn.getAttribute("disabled") === null) {
+        // self.form_btn.click();
+      } else {
+
+      }
+    }    
+  }
+
   listenOn(self, e) { 
     this.account_name = self.account_form.value.trim();
     if (this.isInvalidname(this.account_name)) {
       self.form_btn.setAttribute("disabled", true);
+      self.label_alt.textContent = "invalid name.";
       if (this.account_name == "") self.label_alt.textContent = "";
-      else self.label_alt.textContent = "invalid name";
-    } else {
-      self.form_btn.setAttribute("enabled", true);
-      console.log(self.label_alt);
-      self.label_alt.textContent = "";
+      return;
     }
+    self.form_btn.removeAttribute("disabled");
+    self.label_alt.textContent = "";
   }
 
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import checkForm from "./checkForm";
 const sc_account = document.getElementById("sc_account");
 const label_alt = document.getElementById("label_alt");
 const form_btn = document.getElementById("go_btn");
-console.log(label_alt);
-let go_btn = new checkForm(sc_account, form_btn, label_alt);
-// go_btn.listenOn();
+const myform = document.getElementById("myForm");
 
-// なんかようわからんので愚直にやるやつ
 
-/*
-document.getElementById("sc_account").addEventListener("input", (e) => {
-  console.log(document.getElementById("sc_account").value);   
-});
-*/
+let go_btn = new checkForm(sc_account, form_btn, label_alt, myForm);
